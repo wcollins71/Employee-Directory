83ba0d162877b3421ee76a8f93e1255b013fae90
Wayne Collins <wayne.c@tcwa.com.au>
2021-01-16 08:26:43 +0800

    Initial upload with banner

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Banner from './components/banner/banner';
+
+function App() {
+  return (
+    <div className="App">
+  <Banner />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/banner/banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./banner.css";
+
+function Banner () {
+    return (
+      <div className="banner">
+        <h1>Employee Directory</h1>
+        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+      </div>
+    )
+  }
+
+export default Banner;
